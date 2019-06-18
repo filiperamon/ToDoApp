5284581a660413de1be7f484a87df69585088aa7
Filipe <u201737@marquise.com.br>
2019-06-18 17:04:31 -0300

    [TodoApp]
    - Initit config frontend
    - Menu application

---
+++ todo-app/frontend/src/about/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name="About" small="me"/>
+        <h2>History</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Mission</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+    </div>
+)
